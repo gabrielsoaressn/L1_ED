c95b9d9b0fde637647d11319c03d674a064d0eca
Gabriel Soares <64909008+gabrielsoaressn@users.noreply.github.com>
2021-08-18 19:05:30 -0300

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num;
+    cout<<"------PAR OU IMPAR------"<<endl;
+    cout<<"Digite o numero: ";
+    cin >> num;
+    if(num%2==1)
+    {
+      cout <<"numero impar"<<endl;
+    }
+    if(num%2==0)
+    {
+      cout <<"numero par"<<endl;
+    }
+  return 0;
+}
